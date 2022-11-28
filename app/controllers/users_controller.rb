82340349fd5acfe4b545cea70d59079da011be9a
danstan <danstan.oduor@student.moringaschool.com>
2022-11-28 11:20:43 +0300

    displaying associated in an array format

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,11 @@
 class UsersController < ApplicationController
+    
+    def show
+      user = User.find_by(id: params[:id])
+      if(user)
+          render json: user, include: :items, status: :ok
+      else
+          render json: {error: "user not found!"}, status: 404
+      end
+  end
 end
